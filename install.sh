4c52a9e2f5f2ab71746c611e536f61b782e9265b
Sam Weston <weston.sam@gmail.com>
2020-07-26 14:55:02 +0100

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm upgrade --install prometheus stable/prometheus-operator --values prometheus-operator-values.yaml
