cb7454188fca9e65c243072ea006086a0dd49aa8
Sam Weston <weston.sam@gmail.com>
2020-07-26 15:18:11 +0100

    Add traefik servicemonitor

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 helm upgrade --install prometheus stable/prometheus-operator --values prometheus-operator-values.yaml
+kubectl apply -f traefik-servicemonitor.yaml
